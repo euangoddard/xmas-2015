f0e1a7ff7f172df65296dfb9c973a9603c556809
Euan Goddard <euangoddard@2degreesnetwork.com>
2015-04-20 13:26:32 +0100

    Abstracted differences between social signon providers

--- src/js/app.js
+++ src/js/app.js
@@ -37,35 +37,37 @@ app.controller('AuthController', function ($scope, $firebaseAuth) {
   var ref = new Firebase('https://xmas-2015.firebaseio.com/');
   var auth = $firebaseAuth(ref);
 
+  $scope.person = {};
+
   this.login_with_google = function () {
-    $scope.authData = null;
     $scope.error = null;
 
-    auth.$authWithOAuthPopup('google', {scope: ['profile']}).then(function (authData) {
-      $scope.authData = authData;
+    auth.$authWithOAuthPopup('google', {scope: ['profile']}).then(function (auth_data) {
+      $scope.person.name = auth_data.google.displayName;
+      $scope.person.picture_url = auth_data.google.cachedUserProfile.picture;
     }).catch(function(error) {
       $scope.error = error;
     });
   };
 
   this.login_with_twitter = function () {
-    $scope.authData = null;
+    $scope.auth_data = null;
     $scope.error = null;
 
-    auth.$authWithOAuthPopup('twitter').then(function (authData) {
-      $scope.authData = authData;
+    auth.$authWithOAuthPopup('twitter').then(function (auth_data) {
+      $scope.person.name = auth_data.twitter.displayName;
+      $scope.person.picture_url = auth_data.twitter.cachedUserProfile.profile_image_url_https;
     }).catch(function(error) {
       $scope.error = error;
     });
   };
 
   this.login_with_facebook = function () {
-    $scope.authData = null;
     $scope.error = null;
 
-    auth.$authWithOAuthPopup('facebook', {scope: ['public_profile']}).then(function (authData) {
-      $scope.authData = authData;
-      console.table(authData.facebook.cachedUserProfile);
+    auth.$authWithOAuthPopup('facebook', {scope: ['public_profile']}).then(function (auth_data) {
+      $scope.person.name = auth_data.facebook.displayName;
+      $scope.person.picture_url = auth_data.facebook.cachedUserProfile.picture.data.url;
     }).catch(function(error) {
       $scope.error = error;
     });
