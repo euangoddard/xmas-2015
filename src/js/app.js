16f956a08dfe582b506464848b1e12a39f77b41a
Euan Goddard <euan.goddard@gmail.com>
2015-04-26 21:42:55 +0100

    Consolidated social avatar selection

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 var app = angular.module('xmas-2015', ['firebase']);
-return
 
 
 app.controller('MainController', function ($scope, $firebaseArray) {
@@ -26,51 +25,56 @@ app.controller('AvatarController', function ($scope) {
   $scope.avatar = {};
 });
 
-app.factory('Avatar', function () {
-
-
-  return {};
-});
-
-
 app.controller('AuthController', function ($scope, $firebaseAuth) {
   var ref = new Firebase('https://xmas-2015.firebaseio.com/');
   var auth = $firebaseAuth(ref);
 
+  var build_authenticator = function (service_name, service_params) {
+    return function () {
+      auth.$authWithOAuthPopup(service_name, service_params)
+        .then(set_authenticated_user)
+        .catch(set_authentication_error);
+    };
+  };
+
+  this.login_with_google = build_authenticator('google', {scope: ['profile']});
+
+  this.login_with_twitter = build_authenticator('twitter', {});
+
+  this.login_with_facebook = build_authenticator('facebook', {scope: ['public_profile']});
+
+  var set_authenticated_user = function (auth_data) {
+    $scope.error = null;
+    switch (auth_data.provider) {
+      case 'google':
+        $scope.person.name = auth_data.google.displayName;
+        $scope.person.picture_url = auth_data.google.cachedUserProfile.picture;
+        break;
+      case 'facebook':
+        $scope.person.name = auth_data.facebook.displayName;
+        $scope.person.picture_url = auth_data.facebook.cachedUserProfile.picture.data.url;
+        break;
+      case 'twitter':
+        $scope.person.name = auth_data.twitter.displayName;
+        $scope.person.picture_url = auth_data.twitter.cachedUserProfile.profile_image_url_https;
+        break;
+      default:
+        throw new Error('Unknown authentication provider: ' + auth_data.provider);
+    }
+  };
+
+  var set_authentication_error = function (error_message) {
+    $scope.person = {};
+    $scope.error = error;
+  };
+
   $scope.person = {};
-
-  this.login_with_google = function () {
-    $scope.error = null;
-
-    auth.$authWithOAuthPopup('google', {scope: ['profile']}).then(function (auth_data) {
-      $scope.person.name = auth_data.google.displayName;
-      $scope.person.picture_url = auth_data.google.cachedUserProfile.picture;
-    }).catch(function(error) {
-      $scope.error = error;
-    });
-  };
-
-  this.login_with_twitter = function () {
-    $scope.auth_data = null;
-    $scope.error = null;
-
-    auth.$authWithOAuthPopup('twitter').then(function (auth_data) {
-      $scope.person.name = auth_data.twitter.displayName;
-      $scope.person.picture_url = auth_data.twitter.cachedUserProfile.profile_image_url_https;
-    }).catch(function(error) {
-      $scope.error = error;
-    });
-  };
-
-  this.login_with_facebook = function () {
-    $scope.error = null;
-
-    auth.$authWithOAuthPopup('facebook', {scope: ['public_profile']}).then(function (auth_data) {
-      $scope.person.name = auth_data.facebook.displayName;
-      $scope.person.picture_url = auth_data.facebook.cachedUserProfile.picture.data.url;
-    }).catch(function(error) {
-      $scope.error = error;
-    });
-  };
+  $scope.error = null;
+  (function () {
+    var auth_data = auth.$getAuth();
+    if (auth_data) {
+      set_authenticated_user(auth_data);
+    }
+  }());
   
 });
